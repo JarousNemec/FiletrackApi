6af992687d312308df812e7e433c6662bece9873
JarousNemec <jarous.nemec@gmail.com>
2023-11-20 21:16:25 +0100

    Initial commit Project moved in to separate repo

---
+++ FiletrackAPI/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("test/{action}")]
+public class TestController : Controller
+{
+    public string Get()
+    {
+        return $"Time is {DateTime.Now} ";
+    }
+    public DateTime GetTime()
+    {
+        return DateTime.Now;
+    }
+    public int GetRandom()
+    {
+        return Random.Shared.Next(int.MinValue, int.MaxValue);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # FiletrackApi
+
+
+## Autor: Jaroslav Němec T3 SSAKHK 2023/2024
